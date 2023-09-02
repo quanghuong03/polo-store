eb6332e18816e5302f6e6314fbff125079ee17fa
quanghuong03 <phamquanghuong03@gmail.com>
2023-09-02 20:48:22 +0700

    quanghuong

---
+++ polo-store/src/main/java/com/beefat/polostore/PoloStoreApplication.java
@@ -0,0 +1,13 @@
+package com.beefat.polostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoloStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoloStoreApplication.class, args);
+    }
+
+}
---
+++ polo-store/src/test/java/com/beefat/polostore/PoloStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beefat.polostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoloStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
